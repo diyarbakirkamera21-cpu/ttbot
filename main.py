7bee6cadabf7f7d104af5bc2fdc3ad9b3f04fd35
diyarbakirkamera21-cpu <diyarbakirkamera21@gmail.com>
2026-06-11 22:46:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from telethon import TelegramClient, events
 from telethon.sessions import StringSession
 import asyncio
 
-API_ID = 38539979
-API_HASH = "c763435d9e2e6397992c3cf60448ad33"
-STRING_SESSION = "1BJWap1sBu5fPGaQonTVjHfaV82jvH7arf_zQOVD5lXsSYm299ymGfhQ6OUbt5b2vHVa5q65ugMkGAHEY5-5DrwwBOb4ydpby-adhMGuAzdOi3Utm3fI7Xm0DqqyIuyILZheZjCX0xXQ7CL26DZkzNyIT_Fi2J5iEL9u1HxRq3rE30PxI0dtLaTOuYEAL42Q3Q5ySIq4IwjirThlg5GExm3Wf2vFoB6gBUsrWDy2FRXHnZb9PX1IH9Jop2urgRmeC4cNfMM37GtIAhFVpuhfJ-Q_-Na0b0m0ALMr3bDQE486tyjag2rip_XnrX1b0PyaUwkSNX-kdBKnbxkQoqb6OUcGN-wbWWaQ="
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+STRING_SESSION = os.getenv("STRING_SESSION")
 
 SOURCE_CHANNELS = ["@ww3media", "@Sancaktari"]
 TARGET_CHANNEL = "@dunyadanhaberlerdeepweb"
